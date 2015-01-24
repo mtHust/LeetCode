db27f6be275e7eff9f4651b70846a10fa5fa2f7c
mtHust <maotinghust@gmail.com>
2015-01-24 13:27:58 +0800

    largest-number
    Sort
    1. convert number to string
    2. construct the Node objects vector
    3. sort the vector by cmpclass
    4. cmpclass
    compare (str1 + str2) with (str2 + str1)

--- largest-number/largest-number.h
+++ largest-number/largest-number.h
@@ -14,24 +14,19 @@ Special thanks to @ts for adding this problem and creating all test cases.
 
 Hide Tags Sort
 
+[20, 1]
 [128, 12]
 [12, 128]
 [883, 8]
 [2, 2313, 2281]
 
 Method:
+    Sort
     1. convert number to string
     2. construct the Node objects vector
     3. sort the vector by cmpclass
     4. cmpclass
-        1)
-        str1 = strSame + strDiff1;
-        str2 = strSame + strDiff2;
-        samply compare strDiff1 with strDiff2
-        2)
-        str1 = strSame + strDiff;
-        str2 = strSame;
-        compare (strSame + strDiff) with (strDiff + strSame)
+        compare (str1 + str2) with (str2 + str1)
 
 Copyright:
     by maotinghust@gmail.com
@@ -58,8 +53,9 @@ public:
         std::sort(nodes.begin(), nodes.end(), cmpobject);
 
         for(int inode=0; inode<nodes.size(); ++inode){
-            for(int idigit=nodes[inode].digits.size()-1; idigit>=0; --idigit){
-                if(nodes[0].digits[nodes[0].digits.size()-1]=='0'){
+            for(int idigit=0; idigit<nodes[inode].digits.size(); ++idigit){
+                // the highest==0, ex: [0, 0, 0]
+                if(nodes[0].digits[0]=='0'){
                     data.push_back('0');
                     return data;
                 }
@@ -79,52 +75,30 @@ private:
                 digits.push_back('0');
             }
 
+            int sz = 0;
+
             while(num>0){
                 digits.push_back('0' + num%10);
                 num /= 10;
+                ++sz;
+            }
+
+            // reverse
+            for(int i=0; i<sz/2; ++i){
+                char c = digits[i];
+                digits[i] = digits[sz-1-i];
+                digits[sz-1-i] = c;
             }
         }
 
-        string digits;  // digits, reversed
+        string digits;  // digits
     };
 
     class cmpclass {
     public:
         bool operator() (const Node &node1, const Node & node2) {
             // return node1 > node2
-            string strSame;
-            string strDiff;
-            for(int i1=node1.digits.size()-1, i2=node2.digits.size()-1;
-                i1>=0 && i2>=0; --i1, --i2){
-                if(node1.digits[i1] < node2.digits[i2]){
-                    return false;
-                }else if(node1.digits[i1] > node2.digits[i2]){
-                    return true;
-                }else{
-                    if(i1==0 && i2==0)
-                        return false;
-
-                    strSame.push_back(node1.digits[i1]);
-
-                    if(i1==0){
-                        for(int j2=i2-1; j2>=0; --j2){
-                            strDiff.push_back(node2.digits[j2]);
-                        }
-
-                        return (strSame+strDiff) > (strDiff+strSame);
-                    }
-
-                    if(i2==0){
-                        for(int j1=i1-1; j1>=0; --j1){
-                            strDiff.push_back(node1.digits[j1]);
-                        }
-
-                        return (strSame+strDiff) < (strDiff+strSame);
-                    }
-                }
-            }
-
-            return true;
+            return (node1.digits + node2.digits) > (node2.digits + node1.digits);
         }
     } cmpobject;
 };
@@ -137,8 +111,8 @@ void test_largest_number(){
 
     vector<int> num;
 
-    num.push_back(12);
-    num.push_back(128);
+    num.push_back(20);
+    num.push_back(1);
 
     Solution solv;
 
